a5b6afc302f8f81ce0246be5d09e76b30806c386
Thinh Phan <21130548@st.hcmuaf.edu.vn>
2025-04-05 20:38:16 +0700

    Ai cung phai bat dau tu dau do

---
+++ src/main/java/com/example/OnlinePharmacySystem/OnlinePharmacySystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.OnlinePharmacySystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinePharmacySystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlinePharmacySystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/OnlinePharmacySystem/OnlinePharmacySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.OnlinePharmacySystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlinePharmacySystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
