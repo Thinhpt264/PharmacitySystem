0e50cb5739579f1e0c0c0fd89f19cc21b3672944
phucthanh2104 <21130541@st.hcmuaf.edu.vn>
2025-04-11 14:18:12 +0700

    init front end

---
+++ Frontend/Pharmacy/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home.component/home.component';
+import { AboutUsComponent } from './components/aboutUs.component/aboutUs.component';
+import { ServiceComponent } from './components/service.component/service.component';
+import { NewsComponent } from './components/news.component/news.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    AboutUsComponent,
+    ServiceComponent,
+    NewsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
