b457831935ca2f1df38077d90e76847de204e339
Phan Thế Thịnhh <phanthethinh2604@gmail.com>
2025-06-10 22:40:02 +0700

    apply jwt

--- Frontend/Pharmacy/src/app/components/thank-you/thank-you.component.ts
+++ Frontend/Pharmacy/src/app/components/thank-you/thank-you.component.ts
@@ -3,11 +3,14 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './thank-you.component.html',
+  styleUrls: ['./thank-you.component.css'],
 
 })
 export class ThankYouComponent implements OnInit {
     ngOnInit(): void {
      
     }
-  
+    gotoHome() {
+    window.location.href = 'home';
+  }
 }
