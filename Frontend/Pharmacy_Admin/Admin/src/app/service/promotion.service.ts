f4694e596f81f3f7fa392c1dbe5235351e18d095
Trung Tran <21130590@st.hcmuaf.edu.vn>
2025-07-05 00:14:19 +0700

    promotion

--- Frontend/Pharmacy_Admin/Admin/src/app/service/promotion.service.ts
+++ Frontend/Pharmacy_Admin/Admin/src/app/service/promotion.service.ts
@@ -5,15 +5,14 @@ import { lastValueFrom } from 'rxjs';
 // promotion.model.ts
 export enum PromotionType {
   DISCOUNT = 'DISCOUNT',
-  FREE_SHIP = 'FREE_SHIP'
- 
+  FREE_SHIP = 'FREE_SHIP',
 }
 
 export enum PromotionStatus {
   ACTIVE = 'ACTIVE',
   INACTIVE = 'INACTIVE',
-  EXPIRED = 'EXPIRED' ,
-  UPCOMING = 'UPCOMING'
+  EXPIRED = 'EXPIRED',
+  UPCOMING = 'UPCOMING',
 }
 
 export interface PromotionRequestDTO {
@@ -22,7 +21,7 @@ export interface PromotionRequestDTO {
   status: PromotionStatus;
   discountValue: number;
   isPercentageDiscount: boolean;
-  startDate: string; 
+  startDate: string;
   endDate: string;
   quantity: number;
   currentQuantity: number;
@@ -44,47 +43,51 @@ export class PromotionService {
     private baseUrl: BaseUrlService
   ) {}
 
-
-async findAll(): Promise<PromotionResponseDTO[]> {
-  return await lastValueFrom(
-    this.httpClient.get<PromotionResponseDTO[]>(
-      this.baseUrl.getBaseUrl() + 'api/v1/promotions'
-    )
-  );
-}
-
-async findById(id: number): Promise<PromotionResponseDTO> {
-  return await lastValueFrom(
-    this.httpClient.get<PromotionResponseDTO>(
-      this.baseUrl.getBaseUrl() + 'api/v1/promotions/' + id
-    )
-  );
-}
-
-async createPromotion(promo: PromotionRequestDTO): Promise<PromotionResponseDTO> {
-  return await lastValueFrom(
-    this.httpClient.post<PromotionResponseDTO>(
-      this.baseUrl.getBaseUrl() + 'api/v1/promotions',
-      promo
-    )
-  );
-}
-
-async updatePromotion(id: number, promo: PromotionRequestDTO): Promise<PromotionResponseDTO> {
-  return await lastValueFrom(
-    this.httpClient.put<PromotionResponseDTO>(
-      this.baseUrl.getBaseUrl() + 'api/v1/promotions/' + id,
-      promo
-    )
-  );
-}
-async deletePromotion(id: number): Promise<void> { 
+  async findAll(): Promise<PromotionResponseDTO[]> {
     return await lastValueFrom(
-        this.httpClient.delete<void>(
+      this.httpClient.get<PromotionResponseDTO[]>(
+        this.baseUrl.getBaseUrl() + 'api/v1/promotions'
+      )
+    );
+  }
+
+  async findById(id: number): Promise<PromotionResponseDTO> {
+    return await lastValueFrom(
+      this.httpClient.get<PromotionResponseDTO>(
         this.baseUrl.getBaseUrl() + 'api/v1/promotions/' + id
-        )
-    );  
-}
+      )
+    );
+  }
+
+  async createPromotion(
+    promo: PromotionRequestDTO
+  ): Promise<PromotionResponseDTO> {
+    return await lastValueFrom(
+      this.httpClient.post<PromotionResponseDTO>(
+        this.baseUrl.getBaseUrl() + 'api/v1/promotions',
+        promo
+      )
+    );
+  }
+
+  async updatePromotion(
+    id: number,
+    promo: PromotionRequestDTO
+  ): Promise<PromotionResponseDTO> {
+    return await lastValueFrom(
+      this.httpClient.put<PromotionResponseDTO>(
+        this.baseUrl.getBaseUrl() + 'api/v1/promotions/' + id,
+        promo
+      )
+    );
+  }
+  async deletePromotion(id: number): Promise<void> {
+    return await lastValueFrom(
+      this.httpClient.delete<void>(
+        this.baseUrl.getBaseUrl() + 'api/v1/promotions/' + id
+      )
+    );
+  }
 
   cachePromotions(promotions: any[]): void {
     this.promotions = promotions;
@@ -94,7 +97,6 @@ async deletePromotion(id: number): Promise<void> {
     return this.promotions;
   }
 
-  
   async filterPromotions(
     startDate?: string,
     endDate?: string,
@@ -107,14 +109,19 @@ async deletePromotion(id: number): Promise<void> {
 
     if (startDate && endDate) {
       filtered = filtered.filter(
-        promo => promo.startDate >= startDate && promo.endDate <= endDate
+        (promo) => promo.startDate >= startDate && promo.endDate <= endDate
       );
     }
 
     if (type) {
-      filtered = filtered.filter(promo => promo.promotionType === type);
+      filtered = filtered.filter((promo) => promo.promotionType === type);
     }
 
     return filtered;
+  }
+  async getAllProducts(): Promise<any[]> {
+    return await lastValueFrom(
+      this.httpClient.get<any[]>(this.baseUrl.getBaseUrl() + 'api/v1/products')
+    );
   }
 }
